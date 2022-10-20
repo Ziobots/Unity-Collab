01c6645c763c89ff7abbbe396a385be647010490
Ziobots <zionbots@gmail.com>
2022-10-20 01:18:18 -0400

    movement

--- Bullet Collab/Assets/Scripts/Player.cs
+++ Bullet Collab/Assets/Scripts/Player.cs
@@ -1,25 +1,30 @@
+// Make sure to add that thing the teacher wants saying who edit what and when (check slides)
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float horizontal;
-    private float veritcal;
+    Vector2 movement;
 
     public float speed = 8f;
     public Rigidbody2D rb;
 
-    private bool facingRight = true;
-    private bool flipDebounce = true;
+    //private bool facingRight = true;
+    //private bool flipDebounce = true;
 
     // Update is called once per frame
     void Update()
     {
-
+        // Handle Input Here
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate() {
-        
+        // Handle Movement Here
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }

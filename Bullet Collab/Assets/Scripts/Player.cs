acefdaccb3e4b869df812976cccd73fc35b9269f
Ziobots <zionbots@gmail.com>
2022-10-24 17:48:56 -0400

    gun + player flip eff

--- Bullet Collab/Assets/Scripts/Player.cs
+++ Bullet Collab/Assets/Scripts/Player.cs
@@ -8,19 +8,22 @@ public class Player : MonoBehaviour
 {
     // Movement Variables
     public Vector2 movement;
-    public float walkSpeed = 8f;
+    public float walkSpeed = 1f;
     public Rigidbody2D rb;
 
     // Mouse Variables
     public Vector2 mousePosition;
     public Transform arrow;
 
-    //private bool facingRight = true;
+    private bool facingRight = true;
     //private bool flipDebounce = tdrue;
 
     // Update is called once per frame
     void Update()
     {
+        // Update Variables
+        facingRight = (bool)(movement.x > 0);
+
         // Movement Input Here
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -28,25 +31,24 @@ public class Player : MonoBehaviour
         // Mouse Direction Here
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Player Visuals
+        // Arrow
         if (arrow != null){
-            Vector2 arrowPos = (Vector2)transform.position + (mousePosition.normalized);
             Vector2 arrowDir = (mousePosition - (Vector2)arrow.position).normalized;
+            Vector2 arrowPos = (Vector2)transform.position + arrowDir;//(mousePosition.normalized);
             float distance = Vector2.Distance(transform.position,mousePosition);
             
-            if (distance <= 1){
-                arrowPos = arrowPos - arrowDir;// - (mousePosition.normalized);
+            if (distance <= 2f){
+                arrowPos = (Vector2)transform.position - (arrowDir * (1.5f - distance));// - (mousePosition.normalized);
             }
 
-            //arrowPos = (Vector2)transform.position + new Vector2(rightDir.x,0f);
-
             arrow.position = Vector2.Lerp(arrow.position,arrowPos,Time.fixedDeltaTime * 0.5f);
-            arrow.right = arrowDir;
-            print("Data");
-            print(distance);
-            print(arrowDir);
-            arrow.GetComponent<SpriteRenderer>().flipY = (bool)(arrowDir.x < 0);
+            arrow.right = Vector2.Lerp(arrow.right,arrowDir,Time.fixedDeltaTime * 1f);
+
+            //arrow.GetComponent<SpriteRenderer>().flipY = (bool)(arrowDir.x < 0);
         }
+
+        float yRot = Mathf.Lerp(transform.rotation.y,facingRight ? 180 : 0,Time.fixedDeltaTime * 0.5f);
+        transform.Rotate(0,yRot,0);
     }
 
     // Fixed Update is called every physics step

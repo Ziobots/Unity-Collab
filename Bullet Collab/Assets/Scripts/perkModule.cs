e3a8a6996d828f9c8fba0cc2bf04c95fc92153bc
Ziobots <zionbots@gmail.com>
2022-10-30 00:21:52 -0400

    perkModule

---
+++ Bullet Collab/Assets/Scripts/perkModule.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perkModule : MonoBehaviour
+{
+    void applyPerk(List<string> perkIDList,string perkType,Dictionary<string, int> objDictionary){
+
+
+        
+        Dictionary<string, int> editList = new Dictionary<string, int>();
+        editList.Add("Throwing Knife", 3);
+    }
+}
